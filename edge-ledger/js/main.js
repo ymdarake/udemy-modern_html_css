bc5c1eea76a395b9940fd33d290a97c9a368226d
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-15 15:31:51 +1300

    [56]Smooth Scrolling With JS

--- edge-ledger/js/main.js
+++ edge-ledger/js/main.js
@@ -6,3 +6,23 @@ function initMap() {
   });
   const marker = new google.maps.Marker({ position: localStorage, map: map });
 }
+
+window.addEventListener('scroll', function() {
+  document.querySelector('#navbar').style.opacity =
+    this.window.scrollY > 150 ? 0.9 : 1;
+});
+
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
